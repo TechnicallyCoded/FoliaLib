c1653b6d7eace6e02103c7f3e456a22c4ec498ce
TechnicallyCoded <technicallycoded@gmail.com>
2023-07-08 15:45:19 +0200

    Handle isCancelled for legacy Bukkit environments

--- platform/spigot/src/main/java/com/tcoded/folialib/wrapper/task/WrappedBukkitTask.java
+++ platform/spigot/src/main/java/com/tcoded/folialib/wrapper/task/WrappedBukkitTask.java
@@ -2,9 +2,32 @@ package com.tcoded.folialib.wrapper.task;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WrappedBukkitTask implements WrappedTask {
+
+    private static final boolean IS_CANCELLED_SUPPORTED;
+
+    private static Method isCancelledMethod;
+
+    static {
+        boolean supported = false;
+        try {
+            Class<BukkitTask> bukkitTaskClass = BukkitTask.class;
+            //noinspection JavaReflectionMemberAccess
+            isCancelledMethod = bukkitTaskClass.getDeclaredMethod("isCancelled");
+            supported = true;
+        } catch (NoSuchMethodException e) {
+            // ignore
+        }
+
+        // Set class-wide
+        IS_CANCELLED_SUPPORTED = supported;
+    }
 
     private final BukkitTask task;
 
@@ -19,8 +42,27 @@ public class WrappedBukkitTask implements WrappedTask {
 
     @Override
     public boolean isCancelled() {
+        // The updated way of doing this
+        if (IS_CANCELLED_SUPPORTED)
+            return isCancelledUpdated();
+
+        // Legacy support
+        return isCancelledLegacy();
+    }
+
+    private boolean isCancelledUpdated() {
+        try {
+            return (boolean) isCancelledMethod.invoke(this.task);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean isCancelledLegacy() {
         int taskId = this.task.getTaskId();
-        return Bukkit.getScheduler().getPendingTasks().stream().noneMatch(task -> task.getTaskId() == taskId);
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        return !(scheduler.isCurrentlyRunning(taskId) || scheduler.isQueued(taskId));
     }
 
     @Override
